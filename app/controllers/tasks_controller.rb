593575bcec91b33dd658e63896a915bf1922234a
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-11 15:45:33 +0100

    with edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,33 @@ class TasksController < ApplicationController
   end
 
   def show
-    id = params[:id]
-    @task = Task.find(id)
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
