f3a235649af3e09b9177f1dd0c6b2f61aadf96f8
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-11 12:33:34 +0100

    until task list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy all tasks"
+Task.destroy_all
+puts "#{Task.count} in database"
+
+tasks = [
+  {
+    title: 'Laundry',
+    details: 'Do not mix colors!'
+  },
+  {
+    title: 'Studying',
+    details: 'A lot of flashcards to do',
+    completed: true
+  }
+]
+
+puts "Create 2 tasks"
+Task.create(tasks)
+puts "#{Task.count} tasks created"
